efa98f03b8f6a54fcf6196e4f262afdcc56b6e94
Danila Poyarkov <dev@dannote.net>
2026-06-15 19:17:17 +0200

    Mirror source structure in tests

--- test/gpui/host_test.exs
+++ test/gpui/host_test.exs
@@ -1,7 +1,7 @@
 defmodule GPUIHostTest do
   use ExUnit.Case, async: false
 
-  @host Path.expand("../native/gpui_host/target/release/gpui_host", __DIR__)
+  @host Path.expand("native/gpui_host/target/release/gpui_host", File.cwd!())
 
   setup do
     unless File.exists?(@host) do
