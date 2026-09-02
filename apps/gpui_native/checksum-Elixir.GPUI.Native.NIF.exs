d2fa3c29120fbdf78467c12afbbd2a39dfe6e423
Danila Poyarkov <dev@dannote.net>
2026-09-02 10:54:32 +0300

    Add precompiled NIF checksums for v0.2.0-rc.1

---
+++ apps/gpui_native/checksum-Elixir.GPUI.Native.NIF.exs
@@ -0,0 +1,6 @@
+%{
+  "libgpui_nif-v0.2.0-rc.1-nif-2.15-x86_64-unknown-linux-gnu--gpui-component.so.tar.gz" =>
+    "sha256:e401b6e45a5523b24552c5380b10cb26192ba53f593e5b74f8f6378b94857af5",
+  "libgpui_nif-v0.2.0-rc.1-nif-2.15-x86_64-unknown-linux-gnu--vanilla.so.tar.gz" =>
+    "sha256:ad50ac1d450043c0a1cb38568c596019006e0d2e704210e33012cf2d422bf3a6"
+}
