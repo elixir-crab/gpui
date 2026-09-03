71648a525a49b78fbc660477809931a947e58bfe
Danila Poyarkov <dev@dannote.net>
2026-09-03 20:37:34 +0300

    Update precompiled NIF checksums for v0.2.0-rc.2

--- apps/gpui_native/checksum-Elixir.GPUI.Native.NIF.exs
+++ apps/gpui_native/checksum-Elixir.GPUI.Native.NIF.exs
@@ -1,6 +1,14 @@
 %{
-  "libgpui_nif-v0.2.0-rc.1-nif-2.15-x86_64-unknown-linux-gnu--gpui-component.so.tar.gz" =>
-    "sha256:e401b6e45a5523b24552c5380b10cb26192ba53f593e5b74f8f6378b94857af5",
-  "libgpui_nif-v0.2.0-rc.1-nif-2.15-x86_64-unknown-linux-gnu--vanilla.so.tar.gz" =>
-    "sha256:ad50ac1d450043c0a1cb38568c596019006e0d2e704210e33012cf2d422bf3a6"
+  "gpui_nif-v0.2.0-rc.2-nif-2.15-x86_64-pc-windows-msvc--gpui-component.dll.tar.gz" =>
+    "sha256:05bcaf83bac9cfc17dfdb7d15e0f3feb8250502c706234275bb0339ab2c59ff4",
+  "gpui_nif-v0.2.0-rc.2-nif-2.15-x86_64-pc-windows-msvc--vanilla.dll.tar.gz" =>
+    "sha256:1cc869dc5bf2e82ea43f3270a307fab730c3af267f1d5b96e212c0681343fbba",
+  "libgpui_nif-v0.2.0-rc.2-nif-2.15-aarch64-apple-darwin--gpui-component.so.tar.gz" =>
+    "sha256:2f343ecfeae71a52356575b613e4d01b164ddce4ae14ba0fb0d7c3a6e9dc1143",
+  "libgpui_nif-v0.2.0-rc.2-nif-2.15-aarch64-apple-darwin--vanilla.so.tar.gz" =>
+    "sha256:f5a2c29b5eaf768d819869fde02502dfe66fe0b467ab1b3b3d66b6ac15a9a9a5",
+  "libgpui_nif-v0.2.0-rc.2-nif-2.15-x86_64-unknown-linux-gnu--gpui-component.so.tar.gz" =>
+    "sha256:d6acb82e206562a6c79a55f9809c1c5996f254dc0b7f204fd9757cfebfd6b799",
+  "libgpui_nif-v0.2.0-rc.2-nif-2.15-x86_64-unknown-linux-gnu--vanilla.so.tar.gz" =>
+    "sha256:4d8591f81f6961f2327cb0766eca0c7971507d2e76e6659be94b848a09434ee6"
 }
