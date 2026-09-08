d3a0d4f2f77104e2076c9a9cea2d6ea38f9de4f1
Danila Poyarkov <dev@dannote.net>
2026-09-08 16:45:12 +0300

    Record published GPUI 0.2.0 native checksums

--- apps/gpui_native/checksum-Elixir.GPUI.Native.NIF.exs
+++ apps/gpui_native/checksum-Elixir.GPUI.Native.NIF.exs
@@ -1,14 +1,11 @@
 %{
-  "gpui_nif-v0.2.0-rc.2-nif-2.15-x86_64-pc-windows-msvc--gpui-component.dll.tar.gz" =>
-    "sha256:05bcaf83bac9cfc17dfdb7d15e0f3feb8250502c706234275bb0339ab2c59ff4",
-  "gpui_nif-v0.2.0-rc.2-nif-2.15-x86_64-pc-windows-msvc--vanilla.dll.tar.gz" =>
-    "sha256:1cc869dc5bf2e82ea43f3270a307fab730c3af267f1d5b96e212c0681343fbba",
-  "libgpui_nif-v0.2.0-rc.2-nif-2.15-aarch64-apple-darwin--gpui-component.so.tar.gz" =>
-    "sha256:2f343ecfeae71a52356575b613e4d01b164ddce4ae14ba0fb0d7c3a6e9dc1143",
-  "libgpui_nif-v0.2.0-rc.2-nif-2.15-aarch64-apple-darwin--vanilla.so.tar.gz" =>
-    "sha256:f5a2c29b5eaf768d819869fde02502dfe66fe0b467ab1b3b3d66b6ac15a9a9a5",
-  "libgpui_nif-v0.2.0-rc.2-nif-2.15-x86_64-unknown-linux-gnu--gpui-component.so.tar.gz" =>
-    "sha256:d6acb82e206562a6c79a55f9809c1c5996f254dc0b7f204fd9757cfebfd6b799",
-  "libgpui_nif-v0.2.0-rc.2-nif-2.15-x86_64-unknown-linux-gnu--vanilla.so.tar.gz" =>
-    "sha256:4d8591f81f6961f2327cb0766eca0c7971507d2e76e6659be94b848a09434ee6"
+  "gpui_nif-v0.2.0-nif-2.15-x86_64-pc-windows-msvc--gpui-component.dll.tar.gz" => "sha256:88bb9370481c4ce755edf0e4aa9b816fa28e442ee23b81fdf7880df612c71465",
+  "gpui_nif-v0.2.0-nif-2.15-x86_64-pc-windows-msvc--vanilla.dll.tar.gz" => "sha256:1fbed536e7bc06d1cd967ce471a1d3a78fecdbc9bf204b99fa4570508bb295dd",
+  "gpui_nif-v0.2.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:1fbed536e7bc06d1cd967ce471a1d3a78fecdbc9bf204b99fa4570508bb295dd",
+  "libgpui_nif-v0.2.0-nif-2.15-aarch64-apple-darwin--gpui-component.so.tar.gz" => "sha256:72bdce52a38d0185b724143b1de652f3bea56044d658d057cab4ecf042559a3d",
+  "libgpui_nif-v0.2.0-nif-2.15-aarch64-apple-darwin--vanilla.so.tar.gz" => "sha256:ef6838264a3340b020d2c912dbcf7ccc8535c35ca58372b31fd5e6cf64c8d41b",
+  "libgpui_nif-v0.2.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:ef6838264a3340b020d2c912dbcf7ccc8535c35ca58372b31fd5e6cf64c8d41b",
+  "libgpui_nif-v0.2.0-nif-2.15-x86_64-unknown-linux-gnu--gpui-component.so.tar.gz" => "sha256:495ff007668489dd32d3e4d2de7e69d94fbb0884c5ea01a0fda1bb0c647d961f",
+  "libgpui_nif-v0.2.0-nif-2.15-x86_64-unknown-linux-gnu--vanilla.so.tar.gz" => "sha256:55d2033a08b991d3c83aa7149c0859a48de093266b25a2fa87e4ac289f2f3a91",
+  "libgpui_nif-v0.2.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:55d2033a08b991d3c83aa7149c0859a48de093266b25a2fa87e4ac289f2f3a91",
 }
